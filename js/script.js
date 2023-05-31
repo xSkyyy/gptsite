096b51abb001127b459963e28f1433704f2bbd2e
xSkyyy <mc360masteryt@gmail.com>
2023-05-31 01:45:25 +0100

    button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 // This is a placeholder. You can put any js related code here.
+function openModal() {
+    document.getElementById('nsfwModal').style.display = 'block';
+}
+
 function closeModal() {
     document.getElementById('nsfwModal').style.display = 'none';
 }
